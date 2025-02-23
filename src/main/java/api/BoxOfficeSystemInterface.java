38f1213290d2a32dc313a2745da2d00a919dddb1
Daniel Preston <157504913+Daniel-Preston@users.noreply.github.com>
2025-02-23 11:57:00 +0000

    Update BoxOfficeSystemInterface.java

--- src/main/java/api/BoxOfficeSystemInterface.java
+++ src/main/java/api/BoxOfficeSystemInterface.java
@@ -1,5 +1,8 @@
 package api;
 
+import entities.Booking;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -13,5 +16,12 @@ interface BoxOfficeSystemInterface {
     void updateSeatingArrangement(List<String> seatNumbers, List<String> seatStatuses, List<String> rowNumbers, List<Boolean> accessibilityFriendly);
     // Event Attendance Data
     void fetchEventAttendanceData(int eventId);
+    //Book seat for customer
     void createCustomerBooking(String customerName, int seatNumber, boolean accessibilityRequired);
+    //Verify seat data is correct
+    void verifySeatData(int seatBookingID);
+    // Provide list of changes in calendar made by Operations/Marketing
+    void calendarUpdates();
+    //send alert to box office if change is made
+    void sendAlert();
 }
