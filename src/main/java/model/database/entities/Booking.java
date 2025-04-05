8ee96aed9e77c1450c9767ae1459fb9b0333f2f1
Danylo Piatyhorets <Danylo.Piatyhorets@city.ac.uk>
2025-04-05 15:03:42 +0100

    refactor: one booking type

--- src/main/java/model/database/entities/Booking.java
+++ src/main/java/model/database/entities/Booking.java
@@ -1,4 +1,25 @@
 package model.database.entities;
 
+import java.time.LocalDateTime;
+
 public class Booking {
+    //the unique ID that is given to each booking
+    protected String id;
+    //the bookings are under one name
+    protected Customer customer;
+    //the runs have a start date
+    protected LocalDateTime startDate;
+    //the runs have an end date
+    protected LocalDateTime endDate;
+    //the booking has to be confirmed within 28 days!!"!!!
+    protected boolean confirmed;
+
+
+    public Booking(String id, Customer customer, LocalDateTime startDate, LocalDateTime endDate, boolean confirmed) {
+        this.id = id;
+        this.customer = customer;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.confirmed = confirmed;
+    }
 }
--- src/main/java/model/database/entities/booking/BookingType.java
+++
@@ -1,5 +0,0 @@
-package model.database.entities.booking;
-
-public enum BookingType {
-    SINGLE, LARGE, EVENT, WHOLE_VENUE;
-}
